3a28fb711e117b55386a90982813afc194625151
azizamari <azizamari2003@gmail.com>
2020-10-26 19:13:30 +0100

    ability to tweet

--- src/Post.js
+++ src/Post.js
@@ -1,5 +1,5 @@
 import { Avatar } from '@material-ui/core'
-import React from 'react'
+import React, {forwardRef} from 'react'
 import "./Post.css"
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
@@ -7,9 +7,9 @@ import RepeatIcon from '@material-ui/icons/Repeat';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import PublishIcon from '@material-ui/icons/Publish';
 
-function Post({displayName, username, verified, text, image, avatar}) {
+const Post=forwardRef(({displayName, username, verified, text, image, avatar},ref) => {
     return (
-        <div className="post">  
+        <div className="post" ref={ref}>  
             <div className="post__avatar"> 
             <Avatar src={avatar} />
             </div>
@@ -37,6 +37,6 @@ function Post({displayName, username, verified, text, image, avatar}) {
             </div>
         </div>
     )
-}
+});
 
 export default Post
--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,22 +1,59 @@
-import { Button, Avatar } from '@material-ui/core'
-import React from 'react'
-import "./TweetBox.css"
-import aziz from "./aziz.jpg"
+import React, { useState } from "react";
+import "./TweetBox.css";
+import { Avatar, Button } from "@material-ui/core";
+import db from "./firebase";
 
 function TweetBox() {
-    const[twee]
-    return (
-        <div className="tweetbox">
-            <form>
-                <div className="tweetbox__input">
-                    <Avatar src={aziz} />
-                    <input placeholder="What's happening?" type="text" />
-                </div>
-                <input className="tweetBox__imageInput" placeholder="Optional: Enter Image URL" type="text" />
-                <Button className="tweetbox__tweetButton">Tweet</Button>
-            </form>
+const [tweetMessage, setTweetMessage] = useState("");
+const [tweetImage, setTweetImage] = useState("");
+
+const sendTweet = (e) => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+    displayName: "Aziz Amari",
+    username: "@aziz._.amari",
+    verified: true,
+    text: tweetMessage,
+    image: tweetImage,
+    avatar:
+        "https://p16-sg.tiktokcdn.com/img/musically-maliva-obj/1663336038010885~c5_720x720.jpeg",
+});
+
+    setTweetMessage("");
+    setTweetImage("");
+};
+
+return (
+    <div className="tweetBox">
+    <form>
+        <div className="tweetBox__input">
+        <Avatar src="https://p16-sg.tiktokcdn.com/img/musically-maliva-obj/1663336038010885~c5_720x720.jpeg" />
+        <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"
+        />
         </div>
-    )
+        <input
+        value={tweetImage}
+        onChange={(e) => setTweetImage(e.target.value)}
+        className="tweetBox__imageInput"
+        placeholder="Optional: Enter image URL"
+        type="text"
+        />
+
+        <Button
+        onClick={sendTweet}
+        type="submit"
+        className="tweetBox__tweetButton"
+        >
+        Tweet
+        </Button>
+    </form>
+    </div>
+);
 }
 
-export default TweetBox
+export default TweetBox;
